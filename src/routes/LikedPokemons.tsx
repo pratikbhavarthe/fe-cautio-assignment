009a2418b241da313b128c787e63fbc18cd0945a
Pratik Bhavarthe <pratikbhavarthee1998@gmail.com>
2024-06-03 01:23:17 +0530

    Modify src/routes/LikedPokemons.tsx

--- src/routes/LikedPokemons.tsx
+++ src/routes/LikedPokemons.tsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 import PokemonCard, { TPokemonCardProps } from "../components/PokemonCard"; // Import PokemonCard component and its props type
-import { useLikesContext } from "../hooks/useLikesContext"; // Custom hook to access favourites context
-import { paths } from "../constatnts" // Constants for route paths
+import { useFavouritesContext } from "../hooks/useFavouritesContext"; // Custom hook to access favourites context
+import { paths } from "../constants" // Constants for route paths
 
 const LikedPokemons = () => {
-  const { likes } = useLikesContext(); // Retrieve favourites array from context
+  const { favourites } = useFavouritesContext(); // Retrieve favourites array from context
 
   return (
     <div>
@@ -14,7 +14,7 @@ const LikedPokemons = () => {
       </Link>
       {/* Display favourite Pokemon cards in a flex container */}
       <div className="flex flex-wrap gap-4 justify-center">
-        {likes.map((pokemon, index) => (
+        {favourites.map((pokemon, index) => (
           // Render PokemonCard component in view mode only
           <PokemonCardWrapper key={index} data={pokemon} onlyViewMode={false} />
         ))}
