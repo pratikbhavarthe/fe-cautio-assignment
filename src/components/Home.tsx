aaef21eaf44eeb2ae4a2a5066c018b4be62b7869
Pratik Bhavarthe <pratikbhavarthee1998@gmail.com>
2024-06-03 01:23:15 +0530

    Modify src/components/Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,29 +5,23 @@ const Home = () => {
     return (
         <div className="grid place-content-center h-full">
             <motion.div initial={{ scale: 0.9, opacity: 0 }} animate={{ scale: 1, opacity: 1 }}
-                className="w-[min(90vw,380px)] h-[min(90vh,300px)] bg-slate-50 px-8 bg-opacity-95 shadow-lg flex flex-col justify-evenly items-center rounded-3xl">
+                className="w-[min(90vw,380px)] h-[min(90vh,300px)] bg-red-200 px-8 bg-opacity-95 shadow-lg flex flex-col justify-evenly items-center rounded-3xl">
 
-                    <div className="grid place-content-center mt-3">
-                        <img src="/pokeswipe.png" alt="PokéSwipe Logo" className="w-40 h-auto object-contain"/>
-                    </div>
+                <div className="grid place-content-center mt-3">
+                    <img src="/pokeswipelogo.png" alt="PokéSwipe Logo" className="w-40 h-auto object-contain"/>
+                </div>
 
-                    <div className="w-full text-center">
+                <div className="w-full text-center">
                     <h4 className="my-1 text-xl">Here's how to play PokéSwipe</h4>
                     <p className="text-sm">Pokémon show up one by one</p>
                     <p className="text-sm">Swipe or tap the "Like" or "Dislike" Button.</p>
                     <p className="text-sm">Create your dream team</p>
-                    </div>
+                </div>
 
-                    <Link to="/swipe" className="w-full px-2 py-3 text-center block cursor-pointer rounded-xl mx-14"
-                    style={{
-                        backgroundColor: "#12EB90", 
-                        color: "#FFFFFF", 
-                        ":hover": {
-                            backgroundColor: "#32FCA7"
-                        }
-                    }}
-                    >Let's Start</Link>
-                </motion.div>
+                <Link to="/swipe" className="w-full px-2 py-3 text-center block cursor-pointer rounded-xl mx-14 bg-[#12EB90] text-white hover:bg-[#32FCA7]">
+                    Let's Start
+                </Link>
+            </motion.div>
         </div>
     );
 };
