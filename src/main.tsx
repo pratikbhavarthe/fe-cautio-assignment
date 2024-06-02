edffb0624593ad5b84856d527750ad7ee633c64c
Pratik Bhavarthe <pratikbhavarthee1998@gmail.com>
2024-06-03 01:23:16 +0530

    Modify src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./components/App.tsx";
+import { FavouritesProvider } from "./contexts/FavouritesContext.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+export const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <FavouritesProvider>
+          <App />
+        </FavouritesProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
