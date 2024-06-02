3c6af784914b06c52da737b79cccfe0c9a7a9e49
Pratik Bhavarthe <pratikbhavarthee1998@gmail.com>
2024-06-03 01:23:17 +0530

    Modify tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "media",
   theme: {
+    screens: {
+      xs: "320px",
+      ...defaultTheme.screens,
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
